afa20ad605b3a11c95443d9083a43153249e5ab0
Ivan <ivanvolodin960@gmail.com>
2024-01-20 17:23:08 +0300

    feat: add configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegrafModule } from 'nestjs-telegraf';
+import LocalSession from 'telegraf-session-local';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configurations from './configurations';
+
+const sessions = new LocalSession({ database: 'session_db.json' });
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configurations] }),
+    TelegrafModule.forRoot({
+      middlewares: [sessions.middleware()],
+      token: ''
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
---
+++ src/configurations/index.ts
@@ -0,0 +1,4 @@
+export default () => ({
+  port: process.env.PORT,
+  tg_token: process.env.TG_TOKEN
+});
